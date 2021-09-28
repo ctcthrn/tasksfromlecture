ebcb2b88e371b788663fbdcc7699fe6e4e381ec7
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2021-09-28 21:29:34 +0300

    Update Task5V1.cpp

--- Task5V1.cpp
+++ Task5V1.cpp
@@ -1,3 +1,27 @@
+/*
+3. Голунова 3 % 3 + 1 = 1
+Вариант 1 оператор присваивание  =
+1. Для класса Image реализовать:
+class Image {
+  int* img;
+  int mx,int my;
+public:
+  Image(int tmx, int tmy):mx(tmx),my(tmy){
+    img = new int[mx*my]();
+  }
+  vitual ~Image(); // Реализовать
+  int get(int x,int y) {
+    return img[y*my+x];
+  } // Добавить обработку ошибок при помощи исключений - выбрасывать целочисленное исключение при выходе за границы массива
+  int set(int x,int y,int color) { // Добавить обработку ошибок при помощи исключений  - выбрасывать целочисленное исключение при выходе за границы массива
+    img[y*my+x]=color;
+  }
+  Image operator=(const Image& v)                                  
+};
+2. Реализовать перегрузку одной из операций
+3. Реализовать функцию main и показать использование класса Image
+*/
+
 #include <iostream>
 #include <Windows.h>
 
